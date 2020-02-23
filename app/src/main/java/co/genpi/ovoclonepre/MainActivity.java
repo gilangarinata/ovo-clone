be3619c6df41c90aec7f7e51b6be645bbbb3dc0f
gilangarinata <gilangarinata@gmail.com>
2020-02-24 05:57:41 +0700

    first commit

---
+++ app/src/main/java/co/genpi/ovoclonepre/MainActivity.java
@@ -0,0 +1,13 @@
+package co.genpi.ovoclonepre;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
